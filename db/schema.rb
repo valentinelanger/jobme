fb2097654c7775cf4ced087274121ab2e3d88001
Valentine Langer <valentine.langer@gmail.com>
2017-05-22 16:54:13 +0200

    did migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522134940) do
+ActiveRecord::Schema.define(version: 20170522144447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170522134940) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "category"
     t.index ["user_id"], name: "index_jobs_on_user_id", using: :btree
   end
 

308467faa14438b3014f58908affd17d2d5042dc
Valentine Langer <valentine.langer@gmail.com>
2017-05-22 16:00:07 +0200

    added validation rules for request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,4 +3,9 @@ class Request < ApplicationRecord
   belongs_to :job
   belongs_to :pro, class_name: 'User', through: :jobs
 
+  validates :start_at, presence: true
+  validates :end_at, presence: true
+  validates :user_id, presence: true
+  validates :job_id, presence: true
+  validates :pro, presence: true
 end

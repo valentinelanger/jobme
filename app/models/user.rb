9c4d278afb6ec82ea0b3db48e4e24f88caf1cb6d
Valentine Langer <valentine.langer@gmail.com>
2017-05-23 12:19:01 +0200

    facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,38 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
+
+  FACEBOOK_GENDER = {
+    'male' => 'man',
+    'female' => 'woman'
+  }
+
+
+  def self.find_for_facebook_oauth(auth)
+    user_params = auth.slice(:provider, :uid)
+    user_params.merge! auth.info.slice(:email, :first_name, :last_name)
+    user_params[:facebook_picture_url] = auth.info.image
+    user_params[:token] = auth.credentials.token
+    user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
+    user_params = user_params.to_h
+    user_params[:sexe] = FACEBOOK_GENDER[auth.extra.raw_info.gender]
+
+
+
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+    user ||= User.where(email: auth.info.email).first # User did a regular sign up in the past.
+
+
+    if user
+      user.update(user_params)
+    else
+      user = User.new(user_params)
+      user.password = Devise.friendly_token[0,20]  # Fake password for validation
+      user.save!
+    end
+
+    return user
+  end
 end

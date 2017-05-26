69394bb81a2015f102aacd0aca71a7aef1d6dad9
Valentine Langer <valentine.langer@gmail.com>
2017-05-26 16:56:58 +0200

    messages ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,25 @@
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
 //= require_tree .
+
+$(function(){
+  if ($('.messages.index').length > 0) {
+    setInterval(function() {
+      var jobId = $('.wrapper').data('jobId');
+      var requestId = $('.wrapper').data('requestId');
+      $.ajax({
+        type: 'GET',
+        url: '/jobs/' + jobId + '/requests/' + requestId + '/messages',
+        success: function(response) {
+          $('.messages-sent').html($(response).find('.messages-sent'));
+          // debugger;
+          // response.forEach(function(repo) {
+          //   var content = $('.form-inputs').html(repo.content).text();
+          //   var the_message = '<li>' + content +  '</li>';
+          //   $('.unstyle').append(the_message);
+          // });
+        }
+      });
+    }, 3000);
+  }
+});
